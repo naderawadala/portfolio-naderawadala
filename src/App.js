8a3b9eaff07a3e1366bb504545abe65579837804
Nader Awadala <naderawadala@gmail.com>
2023-02-28 17:31:05 +0200

    add experience section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home';
 import Header from './components/Header';
 import About from './components/About/About';
 import Skills from './components/Skills';
+import Experience from './components/Experience';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Home/>
       <About/>
       <Skills/>
+      <Experience/>
       </main>
     </div>
   );
---
+++ src/components/Experience/Experience.js
@@ -0,0 +1,114 @@
+import React, { useState } from 'react'
+import "./experience.css"
+
+const Experience = () => {
+    const [showExperience, toggleShowExperience] = useState(1)
+
+    const toggleTab = (index) => {
+        toggleShowExperience(index)
+    }
+
+    return (
+        <section className='experience section' id="experience">
+            <h2 className='section__title'>Experience</h2>
+            <span className='section__subtitle'>My professional journey</span>
+
+            <div className='experience__container container'>
+                <div className='experience__tabs'>
+                    <div className={showExperience === 1
+                        ? 'experience__button experience__active button--flex'
+                        : 'experience__button button--flex'}
+                        onClick={() => toggleTab(1)}>
+                        <i className='uil uil-graduation-cap experience__icon'></i> Education
+                    </div>
+
+                    <div className={showExperience === 2
+                        ? 'experience__button experience__active button--flex'
+                        : 'experience__button button--flex'}
+                        onClick={() => toggleTab(2)}>
+                        <i className='uil uil-briefcase experience__icon'></i> Work
+                    </div>
+                </div>
+
+                <div className='experience__sections'>
+                    <div className={showExperience === 1
+                        ? 'experience__content experience__content-active'
+                        : 'experience__content content'}>
+                        <div className='experience__data'>
+                            <div>
+                                <h3 className='experience__title'>Bachelor of Software Engineering</h3>
+                                <span className='experience__subtitle'>"St. Cyril and St. Methodius"
+                                    University of Veliko Tarnovo</span>
+                                <div className='experience__calendar'>
+                                    <i className='uil uil-calendar-alt'>2018-2022</i>
+                                </div>
+                            </div>
+
+                            <div>
+                                <span className='experience__rounder'></span>
+                                <span className='experience__line'></span>
+                            </div>
+                        </div>
+
+                        <div className='experience__data'>
+                            <div></div>
+
+                            <div>
+                                <span className='experience__rounder'></span>
+                                <span className='experience__line'></span>
+                            </div>
+
+                            <div>
+                                <h3 className='experience__title'>High School Diploma</h3>
+                                <span className='experience__subtitle'>Secondary School "Emiliyan Stanev"</span>
+                                <div className='experience__calendar'>
+                                    <i className='uil uil-calendar-alt'>2013-2018</i>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className={showExperience === 2
+                        ? 'experience__content experience__content-active'
+                        : 'experience__content content'}>
+                        <div className='experience__data'>
+                            <div>
+                                <h3 className='experience__title'>Software Engineer</h3>
+                                <span className='experience__subtitle'>pressrelations</span>
+                                <div className='experience__calendar'>
+                                    <i className='uil uil-calendar-alt'>2022-2023</i>
+                                </div>
+                            </div>
+
+                            <div>
+                                <span className='experience__rounder'></span>
+                                <span className='experience__line'></span>
+                            </div>
+                        </div>
+
+                        <div className='experience__data'>
+                        <div></div>
+                        
+                            <div>
+                                <span className='experience__rounder'></span>
+                                <span className='experience__line'></span>
+                            </div>
+
+                            <div>
+                                <h3 className='experience__title'>Software Engineer Intern</h3>
+                                <span className='experience__subtitle'>clouway Ltd.</span>
+                                <div className='experience__calendar'>
+                                    <i className='uil uil-calendar-alt'>2021-2022</i>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Experience
